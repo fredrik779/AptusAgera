016c01ce308dd9138893cdfd8552d1f60f2de1ec
fredrik779 <56190055+fredrik779@users.noreply.github.com>
2020-04-05 21:00:42 +0200

    Spelling

--- title.php
+++ title.php
@@ -19,7 +19,7 @@
 <div class="extrasTitleHeader">Välkommen till Brf Kungen 3!</div>
 <img src="img/pin_map_icon&16.png"> Tornslingan 35<br>
 <img src="img/mail_2_icon&16.png"> info@kungen3.se<br>
-<img src="img/globe_2_icon&16.png"> info@kungen3.se<br>
+<img src="img/globe_2_icon&16.png"> kungen3.se<br>
 <img src="img/calendar_2_icon&16.png"> <?php echo $title ?><br>
 </body>
 </html>
